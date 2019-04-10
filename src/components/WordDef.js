f934ea7f2d69ba78af80ff0274a8c958b66c6ae3
unknown <jankkoziel@gmail.com>
2019-04-10 19:30:15 +0200

    Further styling, words-definition component working properly

--- src/components/WordDef.js
+++ src/components/WordDef.js
@@ -6,20 +6,22 @@ const Word = ({data}) => {
     console.log(data)
     let formatData = (data) =>{
         return {
-            word:data.hwi?data.hwi.hw:'',
+            word:data.hwi?data.hwi.hw.replace(/\*/g,"•"):'',
             date:data.date ? data.date.indexOf('{')!==-1?data.date.substring(0,data.date.indexOf('{')) :  data.date:'No date',
             grFunction:data.fl,
-            shortdef:data.shortdef,
-
+            shortdef:data.shortdef[0]?data.shortdef.map((e)=>e.replace(/^[—–]/g,"")):[`${data.cxs[0].cxl} ${data.cxs[0].cxtis[0].cxt}`],
         }
     }
     let fData = formatData(data);
-
     return (
         <S.Word>
-            <h4>{fData.word} <span> - {fData.grFunction}</span></h4>
-            <p>{fData.date}</p>
-            <p>{fData.shortdef[0]}</p>
+            <S.WordHeader>{fData.word} {fData.grFunction? <span>  ({fData.grFunction})</span>:null}</S.WordHeader>
+            <S.DateSpan>{fData.date}</S.DateSpan>
+            {
+                fData.shortdef.map((e,index)=>{
+                    return <S.Definition key={index}>{index+1} - {e}</S.Definition>
+                })
+            }
         </S.Word>
     );
 };
--- src/styledComponents/all.js
+++ src/styledComponents/all.js
@@ -89,24 +89,56 @@ export let Recom = styled.div`
     border: 1px solid grey;
     border-radius:2px;
     margin:5px;
-    cursor:grab;
+    &:hover{
+        cursor:pointer;
+        transform:scale(1.1);
+    }
 `
 export let WordsContainer = styled.div`
     display:grid;
     grid-column:2/3;
     grid-row-gap:1em;
+    margin:2em 0;
+    grid-template-columns:1fr 1fr;
+    grid-column-gap:1em;
+    @media (max-width:760px){
+        grid-template-columns:1fr
+    }
 `
 export let Word = styled.div`
-
-    padding:10px 20px;
     border-radius:5px;
     box-shadow:0px 0px 2px rgba(0,0,0,0.5);
-    height:200px;
+    border:1px dashed black;
     display:flex;
     align-items:flex-start;
     flex-direction:column;
     justify-content:flex-start;
-    &:first-child{
-        margin-top:2em;
+    padding:10px 15px 20px 15px;
+
+`
+
+export let WordHeader = styled.h4`
+    font-size:1.2rem;
+    margin:0;
+    padding:0;
+    
+    &::first-letter{
+        text-transform:uppercase;
     }
+    >span{
+        font-weight:200;
+        font-size:0.8rem;
+        font-style:italic;
+        color:grey;
+    }
+`
+export let DateSpan = styled.p`
+    font-size:0.7rem;
+    font-weight:200;
+    margin:0;
+`
+
+export let Definition = styled.p`
+    margin:0;
+
 `
