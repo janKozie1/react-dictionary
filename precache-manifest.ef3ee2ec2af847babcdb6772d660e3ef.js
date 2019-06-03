3fa6a4781dd597179501397a1135a340694c1e8c
janKozie1 <jankkoziel@gmail.com>
2019-06-03 23:15:06 +0200

    Updates

---
+++ precache-manifest.ef3ee2ec2af847babcdb6772d660e3ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "86edd31769552868ff7e3924599411d1",
+    "url": "/react-dictionary/static/media/search.86edd317.svg"
+  },
+  {
+    "revision": "0a06184bd1661a6278b8",
+    "url": "/react-dictionary/static/js/runtime~main.71ff301d.js"
+  },
+  {
+    "revision": "cb9f53637d68207b3843",
+    "url": "/react-dictionary/static/js/main.1f9fa2dd.chunk.js"
+  },
+  {
+    "revision": "c7d8a9379b2ecedb756f",
+    "url": "/react-dictionary/static/js/2.dee061d6.chunk.js"
+  },
+  {
+    "revision": "5635b9d6942c6134e1825fc66628bf4d",
+    "url": "/react-dictionary/index.html"
+  }
+];
